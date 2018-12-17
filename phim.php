d5a979de5c126992f9434338f56cbe75604dc862
NgocHa98 <44915192+NgocHa98@users.noreply.github.com>
2018-12-18 00:19:08 +0700

    Update phim.php

--- phim.php
+++ phim.php
@@ -1,11 +1,166 @@
 <?php
     include("header.php");
 ?>
-<div id="phim">
+    <br><br>
     <div class="row">
-        <div class="col-md-"
+        <div class="col-md-4"></div>
+        <div class="col-md-8">
+        <a href="#phimdangchieu" class="btn btn-info" style="font-size: 25px">Phim đang chiếu</a> 
+        <a href="#phimsapchieu" class="btn btn-info" style="font-size: 25px">Phim sắp chiếu</a>
+        </div>
     </div>
+    <br> <br>
 
+    <center><h2><b>--PHIM ĐANG CHIẾU--</b></h2> </center>
+    <br><br>
+<div id="phimdangchieu">
+    <div class="row">
+        <div class="col-md-4"> 
+            <center><img src= "./img/nguoinhen.jpg" width="250" height="270" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Spider-man</h4>
+                    <p>
+                        <a href="phimnguoinhen.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+        <div class="col-md-4"> 
+            <center><img src= "./img/Ralph breaks the internet.jpg" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Ralph breaks the internet</h4>
+                    <p>
+                        <a href="phimRalph.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+        <div class="col-md-4"> 
+            <center><img src= "./img/comaytuthan.jpg" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Cỗ máy tử thần</h4>
+                    <p>
+                        <a href="phimcomaytuthan.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4"> 
+            <center><img src= "./img/parkhangseo.jpg" width="250" height="270" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>PARK HANG SEO: Người truyền lửa</h4>
+                    <p>
+                        <a href="phimparkhangseo.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+        <div class = "col-md-4">
+               <center> <img src= "./img/hunter killer.jpg" class="img-thumbnail" alt="">
+                    <div class ="caption">
+                        <h4>Hunter killer</h4>
+                        <p>
+                            <a href="phimmatvu.php" class="btn btn-danger">Chi tiết</a> 
+                            <a href="#" class="btn btn-danger">Đặt vé</a>
+                        </p>
+                    </div>
+                </center>    
+        </div>
+        <div class = "col-md-4">
+            <center> <img src= "./img/gaigialamchieu.jpg" width="250" height="200" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Gái già lắm chiêu</h4>
+                    <p>
+                        <a href="phimgaigia.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>    
+        </div>
+    </div>
+</div>
+
+<br>
+<div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-8">
+        <a href="#phimdangchieu" class="btn btn-info" style="font-size: 25px">Phim đang chiếu</a> 
+        <a href="#phimsapchieu" class="btn btn-info" style="font-size: 25px">Phim sắp chiếu</a>
+        </div>
+</div>
+    <br> <br>
+<center><h2><b>--PHIM SẮP CHIẾU--</b></h2> </center>
+<br><br>
+
+<div id="phimsapchieu">
+    <div class="row">
+        <div class="col-md-4"> 
+            <center><img src= "./img/marypoppins.jpg" width="250" height="270" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Mary poppins returns</h4>
+                    <p>
+                        <a href="phimmarypoppins.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+        <div class="col-md-4"> 
+            <center><img src= "./img/bumblebee.jpg" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Bumblebee</h4>
+                    <p>
+                        <a href="bumblebee.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+        <div class="col-md-4"> 
+            <center><img src= "./img/dragon.jpg" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>How to train your Dragon</h4>
+                    <p>
+                        <a href="phimdragon.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4"> 
+            <center><img src= "./img/captain.jpg" width="250" height="270" class="img-thumbnail" alt="">
+                <div class ="caption">
+                    <h4>Captain Marvel</h4>
+                    <p>
+                        <a href="phimcaptain.php" class="btn btn-danger">Chi tiết</a> 
+                        <a href="#" class="btn btn-danger">Đặt vé</a>
+                    </p>
+                </div>
+            </center>
+        </div>
+        <div class = "col-md-4">
+               <center> <img src= "./img/iceman2.jpg" width="250" height="270" class="img-thumbnail" alt="">
+                    <div class ="caption">
+                        <h4>Iceman 2</h4>
+                        <p>
+                            <a href="phimiceman2.php" class="btn btn-danger">Chi tiết</a> 
+                            <a href="#" class="btn btn-danger">Đặt vé</a>
+                        </p>
+                    </div>
+                </center>    
+        </div>
+        <div class = "col-md-4">
+            
+        </div>
+    </div>
 </div>
 
 
